65e0a59afec8f6ecbf8a3489230c74efe4c1abe1
ddanya6327 <69433959+ddanya6327@users.noreply.github.com>
2020-11-10 16:43:55 +0900

    add todo action

--- src/Command.ts
+++ src/Command.ts
@@ -1,12 +1,19 @@
 import { waitForInput } from "./Input";
-import { AppState } from "./types";
+import {
+  Action,
+  ActionNewTodo,
+  AppState,
+  Priority,
+  PRIORITY_NAME_MAP,
+} from "./types";
+import { getIsValidEnumValue } from "./util";
 
 export abstract class Command {
   constructor(public key: string, private desc: string) {}
   toString() {
     return `${this.key}: ${this.desc}`;
   }
-  abstract async run(state: AppState): Promise<void>;
+  abstract async run(state: AppState): Promise<void | Action>;
 }
 
 export class CommandPrintTodos extends Command {
@@ -19,5 +26,31 @@ export class CommandPrintTodos extends Command {
       console.log(text);
     }
     await waitForInput("press and key: ");
+  }
+}
+
+export class CommandNewTodo extends Command {
+  constructor() {
+    super("n", "할 일 추가하기");
+  }
+  async run(): Promise<void | ActionNewTodo> {
+    const title = await waitForInput("title: ");
+    // priority 높음(0) ~ 낮음(2)
+    const priorityStr = await waitForInput(
+      `priority ${PRIORITY_NAME_MAP[Priority.High]}(${Priority.High}) ~ ${
+        PRIORITY_NAME_MAP[Priority.Low]
+      }(${Priority.Low}): `
+    );
+    const priority = Number(priorityStr);
+    if (title && CommandNewTodo.getIsPriority(priority)) {
+      return {
+        type: "newTodo",
+        title,
+        priority,
+      };
+    }
+  }
+  static getIsPriority(priority: number): priority is Priority {
+    return getIsValidEnumValue(Priority, priority);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { waitForInput } from "./Input";
 import Todo from "./Todo";
-import { AppState, Priority } from "./types";
-import { Command, CommandPrintTodos } from "./Command";
+import { Action, AppState, Priority } from "./types";
+import { Command, CommandNewTodo, CommandPrintTodos } from "./Command";
 
-const commands: Command[] = [new CommandPrintTodos()];
+const commands: Command[] = [new CommandPrintTodos(), new CommandNewTodo()];
 
 async function main() {
-  const state: AppState = {
+  let state: AppState = {
     todos: [
       new Todo("test1", Priority.High),
       new Todo("test2", Priority.Medium),
@@ -23,9 +23,21 @@ async function main() {
     console.clear();
     const command = commands.find((item) => item.key === key);
     if (command) {
-      await command.run(state);
+      const action = await command.run(state);
+      if (action) {
+        state = getNextState(state, action);
+      }
     }
-    console.log("key: ", key);
   }
 }
 main();
+
+function getNextState(state: AppState, action: Action): AppState {
+  switch (action.type) {
+    case "newTodo":
+      return {
+        ...state,
+        todos: [...state.todos, new Todo(action.title, action.priority)],
+      };
+  }
+}
